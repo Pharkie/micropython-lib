9cd7ff24d98dc0d3fa697a46a5e9f5f02223f004
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2024-01-20 11:53:26 +0000

    python-stdlib/hmac/hmac: Fix to work with hashlib.
    hashlib has no .new method?

--- python-stdlib/hmac/hmac.py
+++ python-stdlib/hmac/hmac.py
@@ -17,8 +17,8 @@ class HMAC:
             make_hash = digestmod  # A
         elif isinstance(digestmod, str):
             # A hash name suitable for hashlib.new().
-            # make_hash = lambda d=b"": hashlib.new(digestmod, d)  # this way does not work with uhashlib, which has no .new method
-            make_hash = lambda d=b"": getattr(hashlib, digestmod)(d) # this way works with uhashlib
+            # make_hash = lambda d=b"": hashlib.new(digestmod, d)  # this way does not work with hashlib, which has no .new method
+            make_hash = lambda d=b"": getattr(hashlib, digestmod)(d) # this way works with hashlib
         else:
             # A module supporting PEP 247.
             make_hash = digestmod.new  # C
